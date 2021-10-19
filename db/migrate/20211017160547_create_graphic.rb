9839aeb46744e42d8389cb1f42b419a8332e53bf
Logan Green <logangreen@logans-air.attlocal.net>
2021-10-18 20:08:24 -0400

    More controllers and keep image stored as base64 image for now

--- db/migrate/20211017160547_create_graphic.rb
+++ db/migrate/20211017160547_create_graphic.rb
@@ -4,6 +4,7 @@ class CreateGraphic < ActiveRecord::Migration[6.1]
       t.string :label
       t.integer :width
       t.integer :height
+      t.string :base64_image
       t.integer :x_position
       t.integer :y_position
       t.timestamps
